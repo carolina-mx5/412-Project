407bf047d7c108a1cc0642144604817954945556
ssdewan <81835258+ssdewan@users.noreply.github.com>
2025-12-05 23:29:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # Database connection settings
 DB_NAME = os.getenv("DB_NAME", "movierec")
-DB_USER = os.getenv("DB_USER", "carolina")  
+DB_USER = os.getenv("DB_USER", "postgres")  
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")  
 DB_HOST = os.getenv("DB_HOST", "localhost")     
 DB_PORT = os.getenv("DB_PORT", "8888")      
